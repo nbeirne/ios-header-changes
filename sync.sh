701fe6290de7f38d2702f38463bb1361c43f2168
Nick <nbeirne@users.noreply.github.com>
2023-06-05 20:24:04 -0400

    Fix sync script

--- sync.sh
+++ sync.sh
@@ -1,2 +1,5 @@
-rsync -vaP --include '*.h' --include="*/" --exclude="*" "$1" .
+XCODE_PATH=$1
+CONTENTS_PATH=$XCODE_PATH/Contents/Developer/Platforms/iPhoneOS.platform
+
+rsync -vaP --include '*.h' --include="*/" --exclude="*" "$CONTENTS_PATH" .
 
